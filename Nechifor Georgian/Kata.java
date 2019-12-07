a871a5510e151c85df34f8be09681a9a434c320a
GeorgianNechifor <nechiforgeorgian@gmail.com>
2019-12-07 15:22:45 +0200

    Delete Kata.java

--- Nechifor Georgian/Kata.java
+++
@@ -1,9 +0,0 @@
-public class Kata {
-
-  public static long divisibleCount(long x, long y, long k) {
-        if (x % k == 0)
-            return (y / k) - (x / k) + 1;
-        return (y / k) - (x / k);
-    }
-  
-}
