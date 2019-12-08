bba502fb39cac8ddb0b48d759db9138011405544
GeorgianNechifor <nechiforgeorgian@gmail.com>
2019-12-08 13:11:09 +0200

    Update Solution.java

--- Nechifor Georgian/Day 28/Solution.java
+++ Nechifor Georgian/Day 28/Solution.java
@@ -1,5 +1,4 @@
 public class Solution {
-
     private boolean isNumber(char c) {
         return c >= '0' && c <= '9';
     }
@@ -7,33 +6,34 @@ public class Solution {
     public int myAtoi(String str) {
         if(str.length() == 0) return 0;
         char[] chrs = str.toCharArray();
-        for(int i = 0; i < chrs.length; i++)
+        
+        for(int i = 0; i < chrs.length; i++) //delete first white spaces
             if(chrs[i] != ' ') {
                 str = str.substring(i, chrs.length);
                 break;
             }
         chrs = str.toCharArray();
-        int sign = -1; // positive;
+        int sign = -1; // positive
         int res = 0;
         int i = 0;
-        if(chrs[0] == '-') {
+        if(chrs[0] == '-') { //check if negative number
             sign = 0;
             i = 1;
         }
-        else if(chrs[0] == '+') {
+        else if(chrs[0] == '+') { //check again if positive
             sign = 1;
             i = 1;
         }
         else if(!isNumber(chrs[0]))
             return 0;
 
-        if(chrs.length == 1 && (sign != -1))
+        if(chrs.length == 1 && (sign != -1)) //check if string contains just the sign
             return 0;
-        if(chrs[0] == '-' && chrs[1] == '+' || (chrs[0] == '+' && chrs[1] == '-'))
+        if(chrs[0] == '-' && chrs[1] == '+' || (chrs[0] == '+' && chrs[1] == '-')) //check for error
             return 0;
         for(; i < chrs.length; i++) {
             if(isNumber(chrs[i])) {
-                long test = (long)res * 10 + (chrs[i] - '0');
+                long test = (long)res * 10 + (chrs[i] - '0'); //check if res overflows int range
                 if(test > Integer.MAX_VALUE) {
                     if(sign == 0) return Integer.MIN_VALUE;
                     return Integer.MAX_VALUE;
@@ -43,7 +43,7 @@ public class Solution {
             else
                  break;
         }
-        if (sign == 0)
+        if (sign == 0) //check the sign
             res = -res;
         return res;
     }
