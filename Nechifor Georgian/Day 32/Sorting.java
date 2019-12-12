53625297fbbb9b833017ab481bc9c6e982582b03
GeorgianNechifor <nechiforgeorgian@gmail.com>
2019-12-12 10:46:06 +0200

    merge sort

---
+++ Nechifor Georgian/Day 32/Sorting.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+public class Sorting {
+    private void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+        
+        for(int i = 0; i < n1; i++)
+            left[i] = arr[l + i];
+        for(int i = 0; i < n2; i++)
+            right[i] = arr[m + 1 + i];
+            
+        int i = 0, j = 0, k = l;
+        while(i < n1 && j < n2) {
+            if(left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else
+                arr[k++] = right[j++];
+        }
+
+        while(i < n1)
+            arr[k++] = left[i++];
+        while(j < n2)
+            arr[k++] = right[j++];
+    }
+
+    public void mergeSort(int[] arr, int l, int r) {
+        if(l < r) {
+            int m = (l + r) / 2;
+
+            mergeSort(arr, l, m);
+            mergeSort(arr, m+1, r);
+
+            merge(arr, l, m, r);
+        }
+    }
+}
