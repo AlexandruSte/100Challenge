67e63dbcf2e41588a109cf09a9057f797f3bd190
GeorgianNechifor <nechiforgeorgian@gmail.com>
2019-12-13 08:36:11 +0200

    merging two sorted lists

--- Nechifor Georgian/Day 33/Solution.java
+++ Nechifor Georgian/Day 33/Solution.java
@@ -6,38 +6,37 @@
  *     ListNode(int x) { val = x; }
  * }
  */
-class Solution {
+public class Solution {
+    //https://leetcode.com/problems/merge-two-sorted-lists/
     private ListNode insertLast(ListNode node, int x) {
         ListNode aux = node;
-        if(node == null) return new ListNode(x);
-        while(aux.next != null) {
+        if (node == null)
+            return new ListNode(x);
+        while (aux.next != null) {
             aux = aux.next;
         }
         aux.next = new ListNode(x);
         return node;
     }
+
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode rez = null;
-        while(l1 != null && l2 != null) {
-            if(l1.val < l2.val) {
-                //rez = new ListNode(l1.val);
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
                 rez = insertLast(rez, l1.val);
                 l1 = l1.next;
             } else {
-//                rez = new ListNode(l2.val);
                 rez = insertLast(rez, l2.val);
                 l2 = l2.next;
             }
         }
 
-        while(l1 != null) {
-//            rez = new ListNode(l1.val);
+        while (l1 != null) {
             rez = insertLast(rez, l1.val);
             l1 = l1.next;
         }
 
-        while(l2 != null) {
-//            rez = new ListNode(l2.val);
+        while (l2 != null) {
             rez = insertLast(rez, l2.val);
             l2 = l2.next;
         }
