60bf022639354e2539777105266d2267c9a5a8ac
GeorgianNechifor <nechiforgeorgian@gmail.com>
2019-12-13 15:54:10 +0200

    Update Easy.java

--- Nechifor Georgian/Day 33/Easy.java
+++ Nechifor Georgian/Day 33/Easy.java
@@ -1,4 +1,49 @@
+class ListNode {
+    int value;
+    ListNode next;
+    ListNode(int x) {
+        value = x;
+    }
+}
+
 public class Easy {
+    
+    //https://leetcode.com/problems/merge-two-sorted-lists/
+    private ListNode insertLast(ListNode node, int x) {
+        ListNode aux = node;
+        if (node == null)
+            return new ListNode(x);
+        while (aux.next != null) {
+            aux = aux.next;
+        }
+        aux.next = new ListNode(x);
+        return node;
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode rez = null;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                rez = insertLast(rez, l1.val);
+                l1 = l1.next;
+            } else {
+                rez = insertLast(rez, l2.val);
+                l2 = l2.next;
+            }
+        }
+
+        while (l1 != null) {
+            rez = insertLast(rez, l1.val);
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            rez = insertLast(rez, l2.val);
+            l2 = l2.next;
+        }
+
+        return rez;
+    }
 
 //https://leetcode.com/problems/happy-number/submissions/
     public boolean isHappy(int n) {
@@ -65,4 +110,6 @@ public class Easy {
         }
         return true;
     }
+    
+    
 }
