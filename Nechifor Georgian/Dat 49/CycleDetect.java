b54087b6ec53270561ff23f4a679316ebf34855b
GeorgianNechifor <nechiforgeorgian@gmail.com>
2020-01-03 12:59:00 +0200

    Create CycleDetect.java

---
+++ Nechifor Georgian/Dat 49/CycleDetect.java
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/linked-list-cycle-ii/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null)
+            return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && slow != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                fast = head;
+                while(fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+
+        }
+        return null;
+    }
+}
