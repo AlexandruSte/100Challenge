3f54f5bdb0ba560819e76f4604887c8e9a88a24b
GeorgianNechifor <nechiforgeorgian@gmail.com>
2020-01-04 10:43:42 +0200

    Update Solution.java

--- Nechifor Georgian/Day 49/Solution.java
+++ Nechifor Georgian/Day 49/Solution.java
@@ -1,1 +1,44 @@
+//https://leetcode.com/problems/linked-list-cycle-ii/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null)
+            return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && slow != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                fast = head;
+                while(fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+
+        }
+        return null;
+    }
+}
+
+
+//https://leetcode.com/problems/kth-largest-element-in-an-array/
+class Solution {
+        public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+}
 
